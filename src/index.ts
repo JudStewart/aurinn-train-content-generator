4aa9e405d8d2754d22cf707be831c1679d57e8f0
Jud Stewart <juds@vt.edu>
2022-04-19 13:22:39 -0400

    Removed storyhooks

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ import Names from "./names";
 import NPCs from "./npcs";
 import Settlements from "./settlements";
 import Establishments from "./establishments";
-import Storyhooks from "./storyhooks";
 import Ben from "./ben";
 
 export {
@@ -13,7 +12,6 @@ export {
   Names,
   NPCs,
   Settlements,
-  Storyhooks,
   Establishments,
   Ben
 };
